056d6d27e009c7133f0d58b75e4ce9da1bdc0911
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2025-03-01 19:25:42 +0530

    static file added

--- loan_management/loan_management/settings.py
+++ loan_management/loan_management/settings.py
@@ -15,7 +15,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'your-default-key')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(" ")
 
 
  
@@ -155,6 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
